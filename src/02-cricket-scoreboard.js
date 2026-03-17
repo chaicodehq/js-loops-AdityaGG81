29d70b7c6a0f7c7178ce206ba8187b103832c7f2
AdityaGG81 <adityaghorpade81@gmail.com>
2026-03-17 12:21:08 +0530

    complete module 01 and 02 with all test passed

--- src/02-cricket-scoreboard.js
+++ src/02-cricket-scoreboard.js
@@ -31,5 +31,32 @@
  *   // => { totalRuns: 0, totalBalls: 0, wickets: 0, fours: 0, sixes: 0 }
  */
 export function cricketScoreboard(balls) {
-  // Your code here
+  if (!Array.isArray(balls) || balls.length === 0)
+    return { totalRuns: 0, totalBalls: 0, wickets: 0, fours: 0, sixes: 0 };
+
+  let totalScore = 0;
+  let totalBalls = 0;
+  let wickets = 0;
+  let fours = 0;
+  let sixes = 0;
+
+  for (let i = 0; i < balls.length; i++) {
+    if (wickets === 10) break;
+    if (balls[i] === -1) {
+      wickets++;
+      totalBalls++;
+      continue;
+    } else if (balls[i] === 4) fours++;
+    else if (balls[i] === 6) sixes++;
+    totalBalls++;
+    totalScore += balls[i];
+  }
+
+  return {
+    totalRuns: totalScore,
+    totalBalls,
+    wickets,
+    fours,
+    sixes,
+  };
 }
