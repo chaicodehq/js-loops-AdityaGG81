fb0e71d8104321b485c0504598b4b0cf400b8672
AdityaGG81 <adityaghorpade81@gmail.com>
2026-03-19 12:59:50 +0530

    complete module 07 and 08 with all tests passed

--- src/08-emi-calculator.js
+++ src/08-emi-calculator.js
@@ -42,5 +42,45 @@
  *   // => { months: -1, totalPaid: -1, totalInterest: -1 }
  */
 export function calculateEMI(principal, monthlyRate, emi) {
-  // Your code here
+  if (
+    typeof principal !== "number" ||
+    typeof monthlyRate !== "number" ||
+    typeof emi !== "number" ||
+    principal <= 0 ||
+    monthlyRate <= 0 ||
+    emi <= 0
+  ) {
+    return { months: -1, totalPaid: -1, totalInterest: -1 };
+  }
+
+  if (emi <= principal * monthlyRate) {
+    return { months: -1, totalPaid: -1, totalInterest: -1 };
+  }
+
+  let remaining = principal;
+  let months = 0;
+  let totalPaid = 0;
+  let totalInterest = 0;
+
+  while (remaining > 0) {
+    const interest = remaining * monthlyRate;
+    remaining += interest;
+    totalInterest += interest;
+
+    if (remaining <= emi) {
+      totalPaid += remaining;
+      remaining = 0;
+    } else {
+      remaining -= emi;
+      totalPaid += emi;
+    }
+
+    months++;
+  }
+  const round = (num) => Math.round(num * 100) / 100;
+  return {
+    months,
+    totalPaid: round(totalPaid),
+    totalInterest: round(totalInterest),
+  };
 }
